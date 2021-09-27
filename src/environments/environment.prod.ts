bf2d3cbcd7105d66a4f203d8db11e651c6979e40
Stéphane <azerot.stephane@gmail.com>
2021-09-27 16:51:39 +0200

    add variable env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   STRIPE_KEY:"pk_test_ZLqttvkZdgqjSwhQRunw8SHj00nC5XQpsm",
-  ONESIGNAL_KEY:"828bfc62-db5d-4078-a66d-e032bcd15563",
   API_URL: "http://green-repack-back.herokuapp.com"
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 export const environment = {
   production: false,
   STRIPE_KEY:"pk_test_ZLqttvkZdgqjSwhQRunw8SHj00nC5XQpsm",
-  ONESIGNAL_KEY:"828bfc62-db5d-4078-a66d-e032bcd15563",
   API_URL: "http://green-repack-back.herokuapp.com"
 
 };
